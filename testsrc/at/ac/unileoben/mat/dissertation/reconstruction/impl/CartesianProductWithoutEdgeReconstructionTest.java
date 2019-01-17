2aab8680dc3e1b51d5df8bc96266a6be14a03d68
Marcin Wardynski <s.mwardynski@gmail.com>
2019-01-17 08:50:03 +0100

    add last test changes

--- testsrc/at/ac/unileoben/mat/dissertation/reconstruction/impl/CartesianProductWithoutEdgeReconstructionTest.java
+++ testsrc/at/ac/unileoben/mat/dissertation/reconstruction/impl/CartesianProductWithoutEdgeReconstructionTest.java
@@ -207,12 +207,12 @@ public class CartesianProductWithoutEdgeReconstructionTest
   static
   {
 
-    examplesList.add(new FactorizationCase("hxK2.txt", 2));
-    examplesList.add(new FactorizationCase("hxP3.txt", 2, 0));
-    examplesList.add(new FactorizationCase("hxP4.txt", 2, 0));
+//    examplesList.add(new FactorizationCase("hxK2.txt", 2));
+//    examplesList.add(new FactorizationCase("hxP3.txt", 2, 0));
+//    examplesList.add(new FactorizationCase("hxP4.txt", 2, 0));
 //    examplesList.add(new FactorizationCase("cubexK2.txt", 4));
 //    examplesList.add(new FactorizationCase("cube-ExK2.txt", 2));
-//    examplesList.add(new FactorizationCase("cube-2ExK2.txt", 2));
+    examplesList.add(new FactorizationCase("cube-2ExK2.txt", 2)); //not working
 //    examplesList.add(new FactorizationCase("cube-2ExP3.txt", 2));
 //    examplesList.add(new FactorizationCase("K23xK2-mirrored.txt", 2));
 //    examplesList.add(new FactorizationCase("notAllEdgesLabeled-root_v3.txt", 2, 3));
@@ -221,15 +221,14 @@ public class CartesianProductWithoutEdgeReconstructionTest
 //    examplesList.add(new FactorizationCase("C3xK2xK2.txt", 3));
 //    examplesList.add(new FactorizationCase("C3xK2.txt", 2));
 //    examplesList.add(new FactorizationCase("C4-ExC4-E.txt", 2));
-//    examplesList.add(new FactorizationCase("handP2.txt", 2));
+//    examplesList.add(new FactorizationCase("handP2.txt", 2)); //WRONG
 //    examplesList.add(new FactorizationCase("S2xK2xK2.txt", 3));
-//    examplesList.add(new FactorizationCase("hxh.txt", 2, 0));
 //    examplesList.add(new FactorizationCase("cube-vxcube-v.txt", 2, 0));
 //    examplesList.add(new FactorizationCase("cube-VxP3.txt", 2, 0));
 //    examplesList.add(new FactorizationCase("S6xP4.txt", 2, 0));
 //    examplesList.add(new FactorizationCase("S3xS3.txt", 2, 0));
-//    examplesList.add(new FactorizationCase("bP3xP6mVbxP3.txt", 2, 0));
-//    examplesList.add(new FactorizationCase("bP3xP6mVbxP4.txt", 2, 0));
+//    examplesList.add(new FactorizationCase("bP3xP6mVbxP3.txt", 2, 0)); //WRONG
+//    examplesList.add(new FactorizationCase("bP3xP6mVbxP4.txt", 2, 0)); //WRONG
 //    examplesList.add(new FactorizationCase("P6xP4.txt", 2, 0));
 //    examplesList.add(new FactorizationCase("P6xP3.txt", 2, 0));
 //    examplesList.add(new FactorizationCase("bP3xK2xK2bmVxP3.txt", 2, 0));
@@ -238,19 +237,20 @@ public class CartesianProductWithoutEdgeReconstructionTest
 //    examplesList.add(new FactorizationCase("K4-ExS2.txt", 2, 10));
 //    examplesList.add(new FactorizationCase("K3pExP3.txt", 2));
 //    examplesList.add(new FactorizationCase("bP3xK2bpExP3.txt", 2, 0));
-//    examplesList.add(new FactorizationCase("bP3xP3bpExP3.txt", 2, 4));
+//    examplesList.add(new FactorizationCase("bP3xP3bpExP3.txt", 2, 4)); //WRONG
 //    examplesList.add(new FactorizationCase("C3xS2.txt", 2));
 //    examplesList.add(new FactorizationCase("C4xS2.txt", 2));
 //    examplesList.add(new FactorizationCase("C4pExS2.txt", 2));
 //    examplesList.add(new FactorizationCase("C6xS2.txt", 2));
 //    examplesList.add(new FactorizationCase("K23xP3.txt", 2, 13));
-//    examplesList.add(new FactorizationCase("handP3.txt", 2));
+//    examplesList.add(new FactorizationCase("handP3.txt", 2)); //WRONG
+//    examplesList.add(new FactorizationCase("hxh.txt", 2, 0));
 //    examplesList.add(new FactorizationCase("hxhxh.txt", 2, 0));
 //    examplesList.add(new FactorizationCase("victory.txt", 2));
     //no square
-    //    examplesList.add(new FactorizationCase("P3xK2.txt", 2));
-    //    examplesList.add(new FactorizationCase("S2xK2.txt", 2));
-    //    examplesList.add(new FactorizationCase("S6xP3.txt", 2, 0));
+//    examplesList.add(new FactorizationCase("P3xK2.txt", 2));
+//    examplesList.add(new FactorizationCase("S2xK2.txt", 2));
+//    examplesList.add(new FactorizationCase("S6xP3.txt", 2, 0));
 
 
   }
